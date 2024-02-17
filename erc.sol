5d7a4241321e6cf165f2ec947a2899df346de206
Utkarsh908 <149078527+Utkarsh908@users.noreply.github.com>
2024-02-17 12:18:05 +0530

    erc.sol

--- erc.sol
+++ erc.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DegenToken is ERC20 {
+    address owner;
+
+    constructor() ERC20("Degen", "DGN") {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+
+    function transferTokens(address to, uint256 amount) public {
+        _transfer(msg.sender, to, amount);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account);
+    }
+}
 
